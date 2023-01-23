7a6625d12f2e9b324353029e4fe6ff60fce7dd44
NamaoySudarat <64030313@kmitl.ac.th>
2023-01-23 16:34:24 +0700

    add part09-12

--- 64030313/Week-02-Lab-01/Week-02-Lab-01/Program.cs
+++ 64030313/Week-02-Lab-01/Week-02-Lab-01/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("{0:#.###}.", 1234.56789);
+﻿Console.Write("Enter your First name: ");
+string fname = Console.ReadLine();
+Console.Write("Enter your Last name: ");
+string lname = Console.ReadLine();
+Console.Write("Enter your ID: ");
+Int32 ID = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter your GPA: ");
+double GPA = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("First name : {0}\n" +
+    "Last name : {1}\n" +
+    "ID : {2}\n" +
+    "GPA : {3}", fname, lname, ID, GPA);
