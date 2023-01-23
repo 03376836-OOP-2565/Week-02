1c76fa2ae3652960d4267f0e9c1afd0b37c4b13c
Nanapon2002 <64030090@kmitl.ac.th>
2023-01-23 21:35:15 +0700

    add part9-12

--- 64030090/Week-02-Lab-01/Week-02-Lab-01/Program.cs
+++ 64030090/Week-02-Lab-01/Week-02-Lab-01/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("{0:#.###}.", 1234.56789);
+﻿using System;
+
+string n,l;
+
+Console.Write("name:");
+n = Console.ReadLine();
+
+Console.Write("Lastname:");
+l = Console.ReadLine();
+
+Console.Write("id : ");
+int id = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("GPA: : ");
+double GPA = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"{n} {l}\nID={id}\nGPA={GPA}");
+Console.ReadKey();
